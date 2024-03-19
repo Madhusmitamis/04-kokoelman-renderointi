88cf0743ff302cbfc37c36a3c84ac4694511e014
Madhusmitamis <148965612+Madhusmitamis@users.noreply.github.com>
2024-03-19 11:08:15 +0200

    Rendering Collections part done

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { useState } from 'react'
 
 const App = (props) => {
   const { notes } = props
-
+  const result = notes.map(note => note.id)
+  console.log(result)
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+      {notes.map(note => 
+      <li key = {note.id}>
+      {note.content}
+      </li>
+      )}
       </ul>
     </div>
   )
+  
 }
 
 export default App
