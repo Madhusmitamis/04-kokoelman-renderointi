c777ef39a540e3855381c9e2af08b8b0233c4187
Madhusmitamis <148965612+Madhusmitamis@users.noreply.github.com>
2024-03-19 12:24:36 +0200

    Refactoring Modules started.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react'
+const Note =({ note }) => {
+  return(
+    <li>{note.content}</li>
+  )
+}
 
-const App = (props) => {
-  const { notes } = props
-  const result = notes.map(note => note.id)
-  console.log(result)
+const App = ({notes}) => {
+  // const { notes } = props
+  // const result = notes.map(note => note.id)
+  // console.log(result)
   return (
     <div>
       <h1>Notes</h1>
       <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
+      <hr></hr>
+      <ul>
       {notes.map(note => 
-      <li key = {note.id}>
-      {note.content}
-      </li>
+      <Note key = {note.id} note = {note}
+      />
       )}
       </ul>
     </div>
